b3f3f4ecb75302389a9732aa88ef34d5bf145f65
rw3078 <98930285+rw3078@users.noreply.github.com>
2022-02-03 19:31:56 -0500

    Add files via upload

--- solution.py
+++ solution.py
@@ -10,9 +10,9 @@ def welcome_assignment_answers(question):
     elif question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "Yes"
+        answer = "No"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
-        answer = "Yes"
+        answer = "No"
     elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
         answer = "42b76fe51778764973077a5a94056724"
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
