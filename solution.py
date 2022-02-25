a1be3ae6168777937b3f623b43bb0680b7ffe479
rw3078 <98930285+rw3078@users.noreply.github.com>
2022-02-25 08:49:29 -0500

    Add files via upload

--- solution.py
+++ solution.py
@@ -4,72 +4,92 @@ from socket import *
 def smtp_client(port=1025, mailserver='127.0.0.1'):
     msg = "\r\n My message"
     endmsg = "\r\n.\r\n"
-    badRequestMessage = "400 Bad Request \r\n"
+    fromHeaderLine = "From: "
+    toHeaderLine = "To: "
 
     # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
-    serverSocket = socket(AF_INET, SOCK_STREAM)
-    serverSocket.bind((mailserver, port))
-    serverSocket.listen()
 
     # Create socket called clientSocket and establish a TCP connection with mailserver and port
 
     # Fill in start
-    clientSocket, addr = serverSocket.accept()
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((mailserver, port))
     # Fill in end
 
     recv = clientSocket.recv(1024).decode()
-    #print(recv) #You can use these print statement to validate return codes from the server.
-    #if recv[:3] != '220':
-    #    print('220 reply not received from server.')
+    # print(recv) #You can use these print statement to validate return codes from the server.
+#     if recv[:3] != '220':
+#         print('220 reply not received from server.')
 
     # Send HELO command and print server response.
-    heloCommand = 'HELO Alice\r\n'
+    heloCommand = 'HELO someschool.edu\r\n'
     clientSocket.send(heloCommand.encode())
     recv1 = clientSocket.recv(1024).decode()
-    #print(recv1) 
-    #if recv1[:3] != '250':
-    #    print('250 reply not received from server.')
+    # print(recv1)
+#     if recv1[:3] != '250':
+#         print('250 reply not received from server.')
 
     # Send MAIL FROM command and handle server response.
     # Fill in start
-    try:
-      mailFromCommand = 'MAIL FROM bob@nyu.edu\r\n'
-      clientSocket.send(mailCommand.encode())
-      recv2 = clientSocket.recv(1024).decode()
+    mailFromCommand = 'MAIL FROM: <alice@someschool.edu>\r\n'
+    clientSocket.send(mailFromCommand.encode())
+    recv2 = clientSocket.recv(1024).decode()
+    # print(recv2)
+#     if recv2[:3] != '250':
+#         print('250 reply not received from server.')
     # Fill in end
 
     # Send RCPT TO command and handle server response.
     # Fill in start
-      rcptToCommand = 'RCPT TO alice@nyu.edu\r\n'
-      clientSocket.send(mailCommand.encode())
-      recv3 = clientSocket.recv(1024).decode()
+    rcptToCommand = 'RCPT TO: <bob@hamburger.edu>\r\n'
+    clientSocket.send(rcptToCommand.encode())
+    recv3 = clientSocket.recv(1024).decode()
+    # print(recv3)
+#     if recv2[:3] != '250':
+#         print('250 reply not received from server.')
     # Fill in end
 
     # Send DATA command and handle server response.
     # Fill in start
-      dataCommand = 'DATA\r\n'
-      clientSocket.send(dataCommand.encode())
-      recv4 = clientSocket.recv(1024).decode()
+    dataCommand = 'DATA\r\n'
+    clientSocket.send(dataCommand.encode())
+    recv4 = clientSocket.recv(1024).decode()
+    # print(recv4)
+#     if recv4[:3] != '354':
+#         print('354 reply not received from server.')
     # Fill in end
 
     # Send message data.
     # Fill in start
-      clientSocket.send(msg.encode())
+    clientSocket.send((fromHeaderLine + "alice@someschool.edu \n" 
+    + toHeaderLine + "bob@hamburger.edu" ).encode())
+    clientSocket.send(msg.encode())
     # Fill in end
 
     # Message ends with a single period, send message end and handle server response.
     # Fill in start
-      clientSocket.send(endmsg.encode())
+    clientSocket.send(endmsg.encode())
+    recv5 = clientSocket.recv(1024).decode()
+    # print(recv5)
+#     if recv5[:3] != '250':
+#         print('250 reply not received from server.')
     # Fill in end
 
     # Send QUIT command and handle server response.
     # Fill in start
-      dataCommand = 'QUIT\r\n'
-      clientSocket.send(dataCommand.encode())
-      recv5 = clientSocket.recv(1024).decode()
-    except IOError:
-      connectionSocket.send(badRequestMessage.encode())
+    quitCommand = 'QUIT\r\n'
+    clientSocket.send(quitCommand.encode())
+    recv6 = clientSocket.recv(1024).decode()
+    # if recv6[:3] != '250':
+#         print(recv6[:3])
+#         print('250 reply not received from server.')
+      
+    clientSocket.close()      
     # Fill in end
+    
+def has_reply_from_server(response_status, expected_status):
+    if response_status != expected_status:
+        print('{} reply not received from server.'.format(expected_status))
 
 
 if __name__ == '__main__':
