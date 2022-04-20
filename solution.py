b76e5899617085284472a7cd3b188b9e38578d1c
rw3078 <98930285+rw3078@users.noreply.github.com>
2022-04-19 23:09:42 -0400

    Add files via upload

--- solution.py
+++ solution.py
@@ -156,11 +156,10 @@ def get_route(hostname):
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
                     #Fill in start
                     #You should add your responses to your lists here and return your list if your destination IP is met
-                    tracelist1.extend((str(ttl), str(round((timeReceived - t) * 1000)) + "ms", addr[0], cur_hostname))
+                    tracelist1.extend((str(ttl), str(round((timeReceived - timeSent) * 1000)) + "ms", addr[0], cur_hostname))
                     tracelist2.append(tracelist1)
-                    if ip == destAddr:
-                        print(tracelist2)
-                        return tracelist2
+                    print(tracelist2)
+                    return tracelist2
                     #Fill in end
                 else:
                     #Fill in start
